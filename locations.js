95314f1c2d5f9f7018fda5917617f0c9f20a8f08
BWatWat <65425702+BWatWat@users.noreply.github.com>
2020-05-22 17:09:00 +0100

    BWatWat/SPIES-/locations.js

---
+++ locations.js
@@ -0,0 +1,27 @@
+var locations = [
+//[ "location1", "role1", "role2", "role3", "role4", "role5", "role6", "role7" ],
+[ "Crusader Army", "Monk", "Imprisoned Saracen", "Servant", "Bishop", "Squire", "Archer", "Knight", "Clown" ],
+[ "WWII Squad", "Resistance Fighter", "Radioman", "Scout", "Medic", "Cook", "Imprisoned Nazi", "Soldier", "Clown" ],
+[ "Hospital", "Nurse", "Doctor", "Anesthesiologist", "Intern", "Therapist", "Surgeon", "Patient", "Clown" ],
+[ "Airplane", "First Class Passenger", "Air Marshall", "Mechanic", "Air Hostess", "Copilot", "Captain", "Economy Class Passenger", "Clown" ],
+[ "Military Base", "Deserter", "Colonel", "Medic", "Sniper", "Officer", "Tank Engineer", "Soldier", "Clown" ],
+[ "Movie Studio", "Stunt Man", "Sound Engineer", "Camera Man", "Director", "Costume Artist", "Producer", "Actor", "Clown" ],
+
+
+[ "White House", "President", "Speaker", "Not a Russian Spy", "Vice President", "Lawyer", "Security Guard", "Russian Diplomat", "Son in Law", "Clown" ],
+[ "WW1 Trench", "German Officer", "German Private", "Grenadier", "Flamethrower dude", "Machine Gun Operator", "Engineer", "Medic", "Clown" ],
+[ "Space Ship", "Captain", "Gunnery Officer", "Coms Officer", "Radar Operator", "Mechanic", "Doctor", "Pilot", "Clown" ],
+[ "Farm", "Farmer", "Worker", "Worker", "Jim Dandy", "Tractor Operator", "Clown" ],
+[ "Band Tour", "Director", "Bus Driver", "Van Driver", "Muscician", "Muscician", "Audience", "Audience", "Clown" ],
+[ "Arkham Asylum", "Dr.Quinzel", "Joker", "Nurse", "The Riddler", "Mr.Freeze", "Security Guard", "Correction Officer", "Clown" ],
+[ "Quarry", "Truck Operator", "Drill Operator", "Security Guard", "Greedy CEO", "Supervisor", "Pyrotechnician", "Rock", "Clown" ],
+[ "Ambulance", "Driver", "Victim", "Paramedic", "Paramedic", "Doctor", "Clown" ],
+[ "Cruise Ship", "Captain", "1st Mate", "Server", "Tourist", "Tourist", "Window Washer", "Newly Wed Couple", "Clown" ],
+[ "Amusement Park", "Director", "Mascot", "Customer", "Tourist", "Electrical Operator", "Repair Man", "Food Court Cook", "Clown" ],
+[ "Pirate Ship", "Stow Away", "Captain", "Deckhand", "First-Mate", "Cook", "Cannoneer", "Clown" ],
+[ "Fishing Boat", "Captain", "Deckhand", "Crane Operator", "Cook", "Mechanic", "Clown" ],
+[ "Ghost Town", "Ghost", "Tour Guide", "Investigator", "Possed Tourist", "Tourist", "Curious Kid", "Worrisome Mom", "Laid Back Dad", "Clown" ],
+[ "Hell", "Lucifer", "Demon", "Succubus", "Imp", "Tortured Soul", "Torturer", "Fiend", "Clown" ], 
+[ "Clown Car", "Clown", "Clown", "Clown", "Clown", "Clown", "Clown", "Clown" ],
+[ "YOU ARE A BALL SACK", "","","","","","","","" ]
+];
